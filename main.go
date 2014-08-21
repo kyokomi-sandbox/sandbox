49f10759385ffc06100795d18324f8723d81e425
kyokomi <kyoko1220adword@gmail.com>
2014-08-22 08:07:31 +0900

    added xlsx sample

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/tealeg/xlsx"
+	"log"
+	"os"
+)
 
 type CliScan struct {
 	Scans []Scan
@@ -31,6 +36,15 @@ func (c *CliScan) scan(key string) string {
 func main() {
 	fmt.Println("Hello Go Sandbox!")
 
+	// ----------------------------------------------------------------
+	// template
+	text := CreateTemplateTree("Hello", "golang").Execute()
+	fmt.Println("Result:", text)
+	// ----------------------------------------------------------------
+	// strings
+	Sample()
+	// ----------------------------------------------------------------
+	// cli-scan
 	c := CliScan {
 		Scans: []Scan{
 			{
@@ -39,21 +53,33 @@ func main() {
 			},
 		},
 	}
-
-//	// template
-//	text := CreateTemplateTree("Hello", "golang").Execute()
-//	fmt.Println("Result:", text)
-//
-//	// strings
-//	Sample()
-
-//	cli.NewApp()
-//	fmt.Println("You don't have any configuration file")
-//	fmt.Print("Do you want to create one? [Y/n]: ")
-//	var answer string
-//	fmt.Scanln(&answer)
-//
-//	fmt.Println("answer >", answer)
 	fmt.Println("answer >", c.scan("answer"))
+	// ----------------------------------------------------------------
+	// Excel
+	xlFile, err := xlsx.OpenFile("_test/sample2.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, sheet := range xlFile.Sheets {
+		// sheet単位でfile生成
+		fmt.Println(sheet.Name)
+		f, err := os.Create("_test/" + sheet.Name + ".txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		// rowはまとめて1行にする
+		for _, row := range sheet.Rows {
+			fmt.Println(row.Cells)
+			for idx, cell := range row.Cells {
+				if idx != 0 {
+					f.WriteString(",")
+				}
+				f.WriteString(cell.String())
+			}
+			f.WriteString("\n")
+		}
+		f.Close()
+	}
 }
 
