675a6f73a90ab77daead98c2f67d2a5de0c45276
kyokomi <kyoko1220adword@gmail.com>
2014-08-23 08:15:47 +0900

    added sample to xlsx, scan-cli, slack

--- main.go
+++ main.go
@@ -1,37 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"github.com/tealeg/xlsx"
-	"log"
-	"os"
-)
-
-type CliScan struct {
-	Scans []Scan
-}
-type Scan struct {
-	Key string
-	Message string
-	Value string
-}
-
-func (c *CliScan) scan(key string) string {
-	var s  Scan
-	for _, s = range c.Scans {
-		if s.Key == key {
-			break
-		}
-	}
-
-	if s.Key != key {
-		return ""
-	}
-
-	fmt.Print(s.Message, ": ")
-	fmt.Scanln(&s.Value)
-	return s.Value
-}
+import "fmt"
 
 func main() {
 	fmt.Println("Hello Go Sandbox!")
@@ -56,30 +25,6 @@ func main() {
 	fmt.Println("answer >", c.scan("answer"))
 	// ----------------------------------------------------------------
 	// Excel
-	xlFile, err := xlsx.OpenFile("_test/sample2.xlsx")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, sheet := range xlFile.Sheets {
-		// sheet単位でfile生成
-		fmt.Println(sheet.Name)
-		f, err := os.Create("_test/" + sheet.Name + ".txt")
-		if err != nil {
-			log.Fatal(err)
-		}
-		// rowはまとめて1行にする
-		for _, row := range sheet.Rows {
-			fmt.Println(row.Cells)
-			for idx, cell := range row.Cells {
-				if idx != 0 {
-					f.WriteString(",")
-				}
-				f.WriteString(cell.String())
-			}
-			f.WriteString("\n")
-		}
-		f.Close()
-	}
+	readExcel()
 }
 
