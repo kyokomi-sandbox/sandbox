903fdec95227e79d37397b60de805b50410a5ab9
kyokomi <kyoko1220adword@gmail.com>
2014-09-14 23:20:02 +0900

    added example http PUT

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"log"
+	"io/ioutil"
+	"strings"
+)
 
-func main() {
-	fmt.Println("Hello Go Sandbox!")
 
-	// ----------------------------------------------------------------
-	// template
+func doExcel() {
+	readExcel()
+}
+
+func doTemplate() {
 	text := CreateTemplateTree("Hello", "golang").Execute()
 	fmt.Println("Result:", text)
-	// ----------------------------------------------------------------
-	// strings
+}
+
+func doString() {
 	Sample()
-	// ----------------------------------------------------------------
-	// cli-scan
+}
+
+func doScan() {
 	c := CliScan {
 		Scans: []Scan{
 			{
@@ -23,8 +33,41 @@ func main() {
 		},
 	}
 	fmt.Println("answer >", c.scan("answer"))
-	// ----------------------------------------------------------------
-	// Excel
-	readExcel()
+}
+
+func doHttpPUT() {
+	params := make(url.Values)
+	params.Set("title", "hogefuga11111111")
+
+	// http put
+	id      := "67040"
+	issueID := "67696"
+	token   := "ssssssssssssssssssssss"
+
+	urlStr := fmt.Sprintf("https://gitlab.com/api/v3/projects/%s/issues/%s?private_token=%s", id, issueID, token)
+
+	req, err := http.NewRequest("PUT", urlStr, strings.NewReader(params.Encode()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(data))
+}
+
+func main() {
+	fmt.Println("Hello Go Sandbox!")
+
+
 }
 
