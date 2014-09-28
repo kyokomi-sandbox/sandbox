f88754ca91bd3301b2a8cea9b2a3f575781945ff
kyokomi <kyoko1220adword@gmail.com>
2014-09-29 07:54:16 +0900

    added gorilla

--- main.go
+++ main.go
@@ -1,35 +1,13 @@
 package main
 
 import (
-	"expvar"
 	"fmt"
-	"net/http"
-	"log"
-	"github.com/gorilla/mux"
-	"runtime"
 )
 
-func numGoroutine() interface{} {
-	return interface{}(runtime.NumGoroutine())
-}
 
 func main() {
 	fmt.Println("Hello Go Sandbox!")
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello")
-	})
-	r.HandleFunc("/hoge/{id}", func (w http.ResponseWriter, r *http.Request) {
-
-		vars := mux.Vars(r)
-
-		fmt.Fprint(w, "hoge Hello id = ", vars["id"])
-	})
-
-	expvar.Publish("goroutineNum", expvar.Func(numGoroutine))
-
-	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	
 }
 
