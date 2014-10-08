7c24240e4a9e625a85d8d48abacef8336c593356
kyokomi <kyoko1220adword@gmail.com>
2014-10-08 11:45:01 +0900

    bytes

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bytes"
+)
 
 func main() {
 	fmt.Println("Hello Go Sandbox!")
 
+	var buf bytes.Buffer
+
+	buf.WriteString("hogehoge")
+	fmt.Println(string(buf.Bytes()))
+
+	bufTitle := bytes.ToTitle(buf.Bytes())
+	fmt.Println(string(bufTitle))
 }
