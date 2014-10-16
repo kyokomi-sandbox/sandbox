afb67473c216cb2e33c57706c1b819d321e28486
kyokomi <kyoko1220adword@gmail.com>
2014-10-16 12:27:00 +0900

    fix: socket.io

--- main.go
+++ main.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello Go Sandbox!")
-	
+
+	socketServer()
 }
---
+++ socket.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	socketio "github.com/googollee/go-socket.io"
+	"log"
+	"net/http"
+)
+
+func socketServer() {
+
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.On("connection", func(so socketio.Socket) {
+		log.Println("on connection")
+		so.Join("chat")
+		so.On("chat message", func(msg string) {
+			if err := so.Emit("chat message", msg); err != nil {
+				log.Println("emit:", err)
+			}
+			so.BroadcastTo("chat", "chat message", msg)
+		})
+		so.On("disconnection", func() {
+			log.Println("on disconnect")
+		})
+	})
+	server.On("error", func(_ socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./assets")))
+	log.Println("Serving at localhost:5000...")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
