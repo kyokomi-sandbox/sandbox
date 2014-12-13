59347c85d90192612fa426b120480431cf619be3
kyokomi <kyoko1220adword@gmail.com>
2014-12-13 23:19:43 +0900

    docomoの雑談API試してみた

--- main.go
+++ main.go
@@ -6,12 +6,22 @@ import (
 	"io"
 	"github.com/zenazn/goji/web"
 	"net/http"
+	"github.com/unrolled/render"
+	"github.com/kyokomi/GoSandbox/docomo"
 )
 
 func main() {
+	rd := render.New()
+	d := docomo.NewDialogue()
+
 	goji.Get("/", func (c web.C, w http.ResponseWriter, r *http.Request) {
 		fmt.Println("hello")
 		io.WriteString(w, "hello")
 	})
+	goji.Get("/zatsudan/:message", func (c web.C, w http.ResponseWriter, r *http.Request) {
+		res := d.Send(c.URLParams["message"])
+		rd.JSON(w, http.StatusOK, string(res))
+	})
+
 	goji.Serve()
 }
