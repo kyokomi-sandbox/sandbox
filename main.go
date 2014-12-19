cecd3904ca01f7d35a6dd14861959191423bb59b
kyokomi <kyoko1220adword@gmail.com>
2014-12-19 18:09:27 +0900

    refactor: mqtt
    configをstructにしてpasswordだけ環境変数にした

--- main.go
+++ main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"fmt"
-	"github.com/zenazn/goji"
 	"io"
-	"github.com/zenazn/goji/web"
 	"net/http"
+
 	"github.com/KyokomiSandbox/GoSandbox/stringer"
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
 )
 
 func main() {
 	textExample()
 	stringer.StringerExample()
+
+	mqttExample()
 }
 
 func server() {
-	goji.Get("/", func (c web.C, w http.ResponseWriter, r *http.Request) {
+	goji.Get("/", func(c web.C, w http.ResponseWriter, r *http.Request) {
 		fmt.Println("hello")
 		io.WriteString(w, "hello")
 	})
--- mqtt.go
+++ mqtt.go
@@ -6,22 +6,34 @@ import (
 
 	"time"
 
+	"os"
+
 	MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
 )
 
+type MQTTConfig struct {
+	host     string
+	port     int
+	user     string
+	password string
+}
+
+var config = MQTTConfig{
+	host:     "lite.mqtt.shiguredo.jp",
+	port:     1883,
+	user:     "kyokomi@github",
+	password: os.Getenv("SANGO_MQTT_PASSWORD"),
+}
+
 func mqttExample() {
-	host := "lite.mqtt.shiguredo.jp"
-	port := 1883
-	user := "handson"
-	password := "handson201412"
 
 	// 接続用の設定を作成します
 	opts := MQTT.NewClientOptions()
-	opts.SetUsername(user)
-	opts.SetPassword(password)
+	opts.SetUsername(config.user)
+	opts.SetPassword(config.password)
 
 	// golangではURIで接続先を指定します。
-	brokerUri := fmt.Sprintf("tcp://%s:%d", host, port)
+	brokerUri := fmt.Sprintf("tcp://%s:%d", config.host, config.port)
 	opts.AddBroker(brokerUri)
 
 	// 設定を元にクライアントを作成します
@@ -33,7 +45,7 @@ func mqttExample() {
 		log.Fatal(err)
 	}
 
-	log.Fatalln(Subscribe(client))
+	log.Fatalln(Subscribe(client, "say"))
 }
 
 func onMessageReceived(client *MQTT.MqttClient, message MQTT.Message) {
@@ -41,16 +53,17 @@ func onMessageReceived(client *MQTT.MqttClient, message MQTT.Message) {
 	fmt.Printf("Message: %s\n", message.Payload())
 
 	if string(message.Payload()) == "ぬるぽ" {
-		Publish(client, "ガッ")
+		Publish(client, "say", "ガッ")
 	}
 }
 
-func Subscribe(client *MQTT.MqttClient) error {
-	topic := "handson/say"
+func Subscribe(client *MQTT.MqttClient, topic string) error {
+	topicName := config.user + "/" + topic
+	// 無料枠はQoS 0のみ
 	qos := 0
 
 	// Subscribeするtopicを設定します
-	topicFilter, err := MQTT.NewTopicFilter(topic, byte(qos))
+	topicFilter, err := MQTT.NewTopicFilter(topicName, byte(qos))
 	if err != nil {
 		return err
 	}
@@ -68,11 +81,11 @@ func Subscribe(client *MQTT.MqttClient) error {
 	}
 }
 
-func Publish(client *MQTT.MqttClient, message string) error {
-	topic := "handson/say"
+func Publish(client *MQTT.MqttClient, topic, message string) error {
+	topicName := config.user + "/" + topic
 	qos := 0
 
-	receipt := client.Publish(MQTT.QoS(qos), topic, message)
+	receipt := client.Publish(MQTT.QoS(qos), topicName, message)
 	<-receipt // Publish成功を待ち受ける
 
 	return nil
