1f76c80b74fce188503a21182f7cdf1f9ad1dc61
kyokomi <kyoko1220adword@gmail.com>
2015-01-11 12:11:21 +0900

    desing template2

--- design/template/template.go
+++ design/template/template.go
@@ -11,7 +11,7 @@ type Display interface {
 type AbstractDisplay struct {
 	display Display
 }
-func newAbstractDisplay(d Display) AbstractDisplay {
+func newDisplay(d Display) AbstractDisplay {
 	return AbstractDisplay{display: d}
 }
 
@@ -27,10 +27,6 @@ type CharDisplay struct {
 
 }
 
-func NewCharDisplay() AbstractDisplay {
-	return newAbstractDisplay(CharDisplay{})
-}
-
 func (d CharDisplay) open() {
 	fmt.Println("---------------")
 }
@@ -43,8 +39,27 @@ func (d CharDisplay) close() {
 
 var _ Display = (*CharDisplay)(nil)
 
+type AstahDisplay struct {
+
+}
+
+func (d AstahDisplay) open() {
+	fmt.Println("*****************")
+}
+func (d AstahDisplay) print() {
+	fmt.Print("d")
+}
+func (d AstahDisplay) close() {
+	fmt.Println("\n*****************")
+}
+
+var _ Display = (*CharDisplay)(nil)
+
 func main() {
-	d := NewCharDisplay()
+	d := newDisplay(CharDisplay{})
 	d.Display()
+
+	a := newDisplay(AstahDisplay{})
+	a.Display()
 }
 
