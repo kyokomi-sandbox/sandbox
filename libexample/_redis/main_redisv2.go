bcd31c40e177893dd897dfb69a4bebec35462ce3
kyokomi <kyoko1220adword@gmail.com>
2015-01-24 23:57:22 +0900

    redis.v2

---
+++ libexample/_redis/main_redisv2.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	redis "gopkg.in/redis.v2"
+	"fmt"
+	"flag"
+	"strings"
+)
+func main() {
+
+	var host string
+	flag.StringVar(&host, "host", "localhost", "host name")
+	flag.Parse()
+
+	opt := redis.Options{}
+	opt.Addr = strings.Join([]string{host, "6379"}, ":")
+	opt.Network = "tcp"
+
+	client := redis.NewClient(&opt)
+	defer client.Close()
+
+	client.Set("foo", "bar")
+
+	strCmd := client.Get("foo")
+	fmt.Println(strCmd.String())
+}
