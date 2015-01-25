2705659f6175585d73e44edde19ff19ac07ea392
kyokomi <kyoko1220adword@gmail.com>
2015-01-25 12:23:51 +0900

    yaml support

--- example/_sql/main_mysql.go
+++ example/_sql/main_mysql.go
@@ -2,33 +2,62 @@ package main
 
 import (
 	"database/sql"
-	"log"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/k0kubun/pp"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/k0kubun/pp"
+	yaml "gopkg.in/yaml.v2"
 )
 
 type Sample struct {
-	ID int
+	ID   int
 	Name string
+}
+type ConfigYaml struct {
+	Database map[string]ConfigDatabase
+}
+
+type ConfigSetting struct {
+	Main map[string]ConfigDatabase
+}
+
+type ConfigDatabase struct {
+	User     string
+	Password string
+	Host     string
+	DB       string
 }
 
 func main() {
 
-	var user string
-	var pass string
-	var host string
-	var dbName string
-	flag.StringVar(&user, "u", "", "user name")
-	flag.StringVar(&pass, "p", "", "password")
-	flag.StringVar(&host, "h", "", "host name")
-	flag.StringVar(&dbName, "D", "", "database name")
+	var configPath string
+	flag.StringVar(&configPath, "path", "./", "config file path")
 	flag.Parse()
 
 	log.SetFlags(log.Llongfile)
 
-	dataSource := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", user, pass, host, dbName)
+	data, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var conf ConfigYaml
+	if err := yaml.Unmarshal(data, &conf); err != nil {
+		log.Fatalln(err)
+	}
+
+	pp.Println(conf)
+
+	dbConf := conf.Database["main"]
+	dataSource := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s",
+		dbConf.User,
+		dbConf.Password,
+		dbConf.Host,
+		dbConf.DB,
+	)
 	db, err := sql.Open("mysql", dataSource)
 	if err != nil {
 		log.Fatalln(err)
