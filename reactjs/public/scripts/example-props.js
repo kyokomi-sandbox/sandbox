338e682487eaf7f8779aa6ccb0c8a6d59bb6577c
kyokomi <kyoko1220adword@gmail.com>
2015-02-01 20:31:45 +0900

    reactjs: componentWillReceiveProps

--- reactjs/public/scripts/example-props.js
+++ reactjs/public/scripts/example-props.js
@@ -4,8 +4,15 @@ var Hello2 = React.createClass({
       tag: "React"
     };
   },
+  // DOM追加時は呼ばれない。このDOMがsetPropsされたり、上書きされるときに呼ばれる
+  componentWillReceiveProps(nextProps) {
+    console.log("componentWillReceiveProps !!!!", nextProps);
+  },
+  componentWillUpdate(nextProps, nextState) {
+    console.log("componentWillUpdate !!!!", nextProps, nextState);
+  },
   render() {
-    return <div>{this.props.tag}:Hello {this.props.name}</div>
+    return <div>{this.props.tag}:Hello {this.props.name} => {this.props.children}</div>
   }
 });
 
@@ -15,5 +22,22 @@ var component = React.render(
 );
 // <div>React:Hello World</div>
 
+// setPropsするので、componentWillReceivePropsが呼ばれる
 component.setProps({ name: "foo" });      // <div>React:Hello foo</div>
+// replacePropsするので、componentWillReceivePropsが呼ばれる
 component.replaceProps({ name: "hoge" }); // <div>:Hello hoge</div>
+
+var idx = 0;
+
+[
+  <Hello2>xxx</Hello2>,
+  <Hello2><span>1</span><span>2</span></Hello2>,
+  <Hello2></Hello2>
+].forEach( jsx => {
+  idx++;
+  // ループ2回目のやつがhello2なので、前のComponentが書き換えられて、componentWillReceivePropsが呼ばれる
+  var children = React.render(jsx, document.getElementById('hello' + idx)).props.children;
+  console.log("#########" + children + "##########");
+  console.log(React.Children.count(children));
+  React.Children.forEach(children, (child) => { console.log(child) });
+});
