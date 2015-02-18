3a954e2b3d94ffc393eacc6df54b69790af4f201
kyokomi <kyoko1220adword@gmail.com>
2015-02-19 07:21:08 +0900

    mp3のdownloadと保存を実装

--- other/yuyuyu/main.go
+++ other/yuyuyu/main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/k0kubun/pp"
+	"os"
 )
 
 type Settings struct {
@@ -21,16 +21,58 @@ var settings = Settings{
 	APIPath: "http://www.onsenradio.info/data/api/getMovieInfo/yuyuyu",
 }
 
+type yuyuyu struct {
+	AllowExpand string        `json:"allowExpand"`
+	Cm          []interface{} `json:"cm"`
+	Copyright   string        `json:"copyright"`
+	Count       string        `json:"count"`
+	Guest       string        `json:"guest"`
+	Link        []struct {
+		ImagePath string `json:"imagePath"`
+		URL       string `json:"url"`
+	} `json:"link"`
+	Mail      string `json:"mail"`
+	MoviePath struct {
+		Android string `json:"Android"`
+		IPhone  string `json:"iPhone"`
+		PC      string `json:"pc"`
+	} `json:"moviePath"`
+	OptionText     string `json:"optionText"`
+	Personality    string `json:"personality"`
+	RecommendGoods []struct {
+		ImagePath string `json:"imagePath"`
+		URL       string `json:"url"`
+	} `json:"recommendGoods"`
+	RecommendMovie []interface{} `json:"recommendMovie"`
+	Schedule       string        `json:"schedule"`
+	ThumbnailPath  string        `json:"thumbnailPath"`
+	Title          string        `json:"title"`
+	Type           string        `json:"type"`
+	Update         string        `json:"update"`
+	URL            string        `json:"url"`
+}
+
 func main() {
 
-	res, err := http.Get(settings.APIPath)
+	yuyu, err := metadata()
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	if err := downloadMovie(yuyu.MoviePath.PC, yuyu.URL, yuyu.Count+".mp3"); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func metadata() (*yuyuyu, error) {
+	res, err := http.Get(settings.APIPath)
+	if err != nil {
+		return nil, err
+	}
+
 	data, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 
 	callback := string(data)
@@ -38,10 +80,40 @@ func main() {
 	callback = strings.Replace(callback, "callback(", "", 1)
 	callback = strings.Replace(callback, ");", "", 1)
 
-	var jsonRaw interface{}
-	if err := json.Unmarshal(bytes.NewBufferString(callback).Bytes(), &jsonRaw); err != nil {
-		log.Fatalln(err)
+	var yuyu yuyuyu
+	if err := json.Unmarshal(bytes.NewBufferString(callback).Bytes(), &yuyu); err != nil {
+		return nil, err
 	}
 
-	pp.Println(jsonRaw)
+	return &yuyu, nil
+}
+
+func downloadMovie(movieURL string, dirPath, fileName string) error {
+
+	res, err := http.Get(movieURL)
+	if err != nil {
+		return err
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
+		log.Println(err)
+	}
+
+	f, err := os.Create(dirPath + "/" + fileName)
+	if err != nil {
+		return err
+	}
+
+	f.Write(data)
+
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	return nil
 }
