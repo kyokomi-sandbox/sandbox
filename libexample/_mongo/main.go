dfb9b84fe058ceee76513765b192719274d70ccf
kyokomi <kyoko1220adword@gmail.com>
2015-04-03 14:49:03 +0900

    mgo/txn検証

--- libexample/_mongo/main.go
+++ libexample/_mongo/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2/txn"
 )
 
 type Person struct {
-	Name string
-	Phone string
+	ID      string
+	Balance int
+	Valid   bool
 }
 
 func main() {
-
+	log.SetFlags(log.Llongfile)
 	session, err := mgo.Dial("localhost")
 	if err != nil {
 		panic(err)
@@ -24,18 +25,40 @@ func main() {
 	// Optional. Switch the session to a monotonic behavior.
 	session.SetMode(mgo.Monotonic, true)
 
-	c := session.DB("test").C("people")
-	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
-		&Person{"Cla", "+55 53 8402 8510"})
-	if err != nil {
-		log.Fatal(err)
+	tc := session.DB("txn_test").C("tc")
+
+	runner := txn.NewRunner(tc)
+	accounts := session.DB("txn_test").C("accounts")
+
+	if err := accounts.Insert(bson.M{"_id": 0, "balance": 300}); err != nil {
+		log.Fatalln(err)
+	}
+	if err := accounts.Insert(bson.M{"_id": 1, "balance": 100}); err != nil {
+		log.Fatalln(err)
+	}
+	if err := accounts.Insert(bson.M{"_id": 2, "balance": 200}); err != nil {
+		log.Fatalln(err)
+	}
+	if err := accounts.Insert(bson.M{"_id": 3, "balance": 400}); err != nil {
+		log.Fatalln(err)
 	}
 
-	result := Person{}
-	err = c.Find(bson.M{"name": "Ale"}).One(&result)
-	if err != nil {
-		log.Fatal(err)
+	ops := []txn.Op{
+		{
+			C:      "accounts",
+			Id:     0,
+			Assert: bson.D{{"$or", []bson.D{{{"balance", 100}}, {{"balance", 300}}}}},
+			Update: bson.D{{"$inc", bson.D{{"balance", 100}}}},
+		},
+		{
+			C:      "accounts",
+			Id:     1,
+			Assert: bson.D{{"balance", 100}},
+			Update: bson.D{{"$inc", bson.D{{"balance", 150}}}},
+		},
 	}
 
-	fmt.Println("Phone:", result.Phone)
+	if err := runner.Run(ops, "", nil); err != nil {
+		log.Fatalln(err)
+	}
 }
