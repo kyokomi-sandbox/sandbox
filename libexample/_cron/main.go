72fdc1fbfe12135cbff28f18d7646b221041e689
kyokomi <kyoko1220adword@gmail.com>
2015-05-07 14:06:53 +0900

    flag管理が必要だった

--- libexample/_cron/main.go
+++ libexample/_cron/main.go
@@ -3,37 +3,66 @@ package main
 import (
 	"fmt"
 
+	"errors"
+	"net/http"
+
+	"github.com/guregu/kami"
 	"github.com/robfig/cron"
-	"time"
-	"github.com/juju/errors"
+	"golang.org/x/net/context"
 )
 
 func main() {
 	c := cron.New()
-	c.AddFunc("5 * * * * *", Hoge) // 毎分5秒
+	c.AddFunc("5 * * * * *", Hoge)   // 毎分5秒
 	c.AddFunc("*/5 * * * * *", Fuga) // 5秒毎
-	c.Start()
-	defer c.Stop()
 
-	time.Sleep(1 * time.Minute)
+	c.Start()
+	isRunning := true
+
+	kami.Get("/", func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+	kami.Get("/start", func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		if !isRunning {
+			c.Start()
+			isRunning = true
+		}
+		w.Write([]byte("START OK"))
+	})
+	kami.Get("/stop", func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		if isRunning {
+			c.Stop()
+			isRunning = false
+		}
+		w.Write([]byte("STOP OK"))
+	})
+
+	defer func() {
+		if isRunning {
+			c.Stop()
+			isRunning = false
+		}
+	}()
+
+	kami.Serve()
 }
 
 func Hoge() {
-	defer func() {
-		r := recover()
-		fmt.Println(r)
-	}()
 	fmt.Println("Every hour on the half hour")
+
+	defer func() {
+		fmt.Println(recover())
+	}()
 
 	panic(errors.New("Hoge errorだよ"))
 }
 
 func Fuga() {
-	defer func() {
-		r := recover()
-		fmt.Println(r)
-	}()
 	fmt.Println("Every hour on the half hour")
+
+	defer func() {
+		fmt.Println(recover())
+	}()
 
 	panic(errors.New("Fuga errorだよ"))
 }
