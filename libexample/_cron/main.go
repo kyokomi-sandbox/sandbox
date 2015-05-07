40be5a65976044517898723e8fc65e0a73677b7e
kyokomi <kyoko1220adword@gmail.com>
2015-05-07 13:33:01 +0900

    cron recover

--- libexample/_cron/main.go
+++ libexample/_cron/main.go
@@ -5,14 +5,35 @@ import (
 
 	"github.com/robfig/cron"
 	"time"
+	"github.com/juju/errors"
 )
 
 func main() {
 	c := cron.New()
-	c.AddFunc("5 * * * * *", func() { fmt.Println("Every hour on the half hour") }) // 毎分5秒
-	c.AddFunc("*/5 * * * * *", func() { fmt.Println("Every hour on the half hour") }) // 5秒毎
+	c.AddFunc("5 * * * * *", Hoge) // 毎分5秒
+	c.AddFunc("*/5 * * * * *", Fuga) // 5秒毎
 	c.Start()
 	defer c.Stop()
 
 	time.Sleep(1 * time.Minute)
+}
+
+func Hoge() {
+	defer func() {
+		r := recover()
+		fmt.Println(r)
+	}()
+	fmt.Println("Every hour on the half hour")
+
+	panic(errors.New("Hoge errorだよ"))
+}
+
+func Fuga() {
+	defer func() {
+		r := recover()
+		fmt.Println(r)
+	}()
+	fmt.Println("Every hour on the half hour")
+
+	panic(errors.New("Fuga errorだよ"))
 }
