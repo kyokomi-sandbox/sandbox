98de8679f9f57a5f245b3f803fd51e9a45467ffa
kyokomi <kyoko1220adword@gmail.com>
2015-09-07 07:04:31 +0900

    5章おわりまで

--- python/helloworld/hello.py
+++ python/helloworld/hello.py
@@ -30,3 +30,34 @@ import re
 
 print(re.match("Hello[ \t]*(.*)world", "Hello     Python world").group(1))
 print(re.match("/(.*)/(.*)/(.*)", "/v2/api/hoge").groups())
+
+D = {"a": 1, "b": 2, "c": 3}
+ks = list(D.keys())
+print(ks)
+ks.sort()
+print(ks)
+
+print("--------------")
+
+for key in ks:
+    print(key, "=>", D[key])
+
+print("--------------")
+
+for key in sorted(D):
+    print(key, "=>", D[key])
+
+squares = [x ** 2 for x in [1, 2, 3, 4, 5]]
+print(squares)
+
+if not "d" in D:
+    print("d ない")
+elif "e" in D:
+    print("d ある e もある")
+else:
+    print("d ある e ない")
+
+f = open("sample.txt", "w")
+f.write("Hello\n")
+f.write("world\n")
+f.close()
