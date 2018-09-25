fd1d0c4cf64576e655e78211e8c774299c21e815
kyokomi <kyoko1220adword@gmail.com>
2018-09-25 14:38:19 +0900

    Remove reactjs

--- reactjs/public/scripts/main.js
+++
@@ -1,8 +0,0 @@
-var React  = require("React");
-
-var CommentBox = React.createFactory(require("./example.js"));
-
-React.render(
-  CommentBox({pollInterval: 2000, url: "comments.json"}),
-  document.getElementById('content')
-);
