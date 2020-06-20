d472cbe39e916295bde8754f71ee2af795b9bb90
kyokomi <email@kyokomi.dev>
2020-06-20 16:25:16 +0900

    Data Binding in Android

---
+++ android/android-databinding/app/src/main/java/com/example/android/databinding/basicsample/data/SimpleViewModel.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.databinding.basicsample.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
+
+/**
+ * A simple VM for [com.example.android.databinding.basicsample.ui.PlainOldActivity].
+ */
+class SimpleViewModel : ViewModel() {
+    private val _name = MutableLiveData("Ada")
+    private val _lastName = MutableLiveData("Lovelace")
+    private var _likes = MutableLiveData(0)
+
+    var name: LiveData<String> = _name
+    var lastName: LiveData<String> = _lastName
+    var likes: LiveData<Int> = _likes
+
+    /**
+     * Increments the number of likes.
+     */
+    fun onLike() {
+        _likes.value = (_likes.value ?: 0) + 1
+    }
+
+    /**
+     * Returns popularity in buckets: [Popularity.NORMAL], [Popularity.POPULAR] or [Popularity.STAR]
+     */
+    // Transformationsを使用して1つのLiveDataを別のLiveDataに依存させることができる
+    val popularity: LiveData<Popularity> = Transformations.map(_likes) { likes ->
+        when {
+            likes > 9 -> Popularity.STAR
+            likes > 4 -> Popularity.POPULAR
+            else -> Popularity.NORMAL
+        }
+    }
+}
+
+enum class Popularity {
+    NORMAL,
+    POPULAR,
+    STAR
+}
