e138c5f4914d6d4e0b53197f4b54e17306bcdb3b
kyokomi <email@kyokomi.dev>
2023-01-09 00:14:21 +0900

    mysqlのrotateを追加

--- golang/private-isu/webapp/check.sh
+++ golang/private-isu/webapp/check.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 alp json --file logs/nginx/access.log
+pt-query-digest logs/mysql/mysql-slow.log
+
--- golang/private-isu/webapp/rotate.sh
+++ golang/private-isu/webapp/rotate.sh
@@ -2,3 +2,5 @@
 
 mv ./logs/nginx/access.log "./logs/nginx/access.log.$(date +%Y%m%d-%H%M%S)"
 docker-compose restart nginx
+mv ./logs/mysql/mysql-slow.log "./logs/mysql/mysql-slow.log.$(date +%Y%m%d-%H%M%S)"
+docker-compose restart mysql
